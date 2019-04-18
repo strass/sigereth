80285b78c3cf8d53344286ff9fb8814b1ef7f545
zak <zakstrassberg@gmail.com>
2019-04-18 01:52:53 +0000

    WIP INITIATIVE WORK

--- src/components/molecules/Combatant/Initiative.tsx
+++ src/components/molecules/Combatant/Initiative.tsx
@@ -1,26 +1,29 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { FunctionComponent, Fragment, useContext } from 'react';
+import { FunctionComponent, useContext } from 'react';
 import { CombatantContext } from '../../context/CombatantContext';
 import NumberSpinner from '../../atoms/NumberSpinner';
 import { toNumber } from 'lodash';
 import CombatantSection from './Section';
+import usePermissions from '../../../hooks/usePermissions';
 
 const CombatantInitiative: FunctionComponent = () => {
-  const combatant = useContext(CombatantContext);
+    const combatant = useContext(CombatantContext);
+    console.log('!', combatant)
+    const { isResourceOwnerOrGameOwner } = usePermissions(combatant);
 
-  return (
-    <CombatantSection title="Initiative">
-      <NumberSpinner
-        value={combatant.data.initiative}
-        onChange={e =>
-          combatant.ref.update({ initiative: toNumber(e.target.value) })
-        }
-        id={`combatant-${combatant.id}-initiative`}
-        css={{ fontSize: '1.6em', border: 'none', padding: 0, margin: 0 }}
-      />
-    </CombatantSection>
-  );
+    return (
+        <CombatantSection title="Initiative">
+            <NumberSpinner
+                value={combatant.data.initiative}
+                onChange={e =>
+                    isResourceOwnerOrGameOwner && combatant.ref.update({ initiative: toNumber(e.target.value) })
+                }
+                id={`combatant-${combatant.id}-initiative`}
+                css={{ fontSize: '1.6em', border: 'none', padding: 0, margin: 0 }}
+            />
+        </CombatantSection>
+    );
 };
 
 export default CombatantInitiative;

f8015bde0d32d18a73102eee51435f78c69529a9
root <root@ip-172-31-63-132.ec2.internal>
2019-04-19 01:12:58 +0000

    config files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false
+};

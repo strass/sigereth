d6b91b79f3607d55934c2c7bb4e9c01ba886b603
root <root@ip-172-31-63-132.ec2.internal>
2019-04-19 01:13:23 +0000

    notes

--- src/DevNotes.tsx
+++ src/DevNotes.tsx
@@ -1,38 +1,51 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import { Collapse } from 'react-collapse';
 
-const DevNotes = () => (
-  <Fragment>
-    <hr />
-    <h2>todo</h2>
-    <ul>
-      <li>style combatant</li>
-      <li>evaluate viability of controlling via scrolling</li>
-      <li>move more updators into useCombatantAction</li>
-      <li>loading control + Suspense</li>
-      <li>react-flip-toolkit</li>
-      <li>
-        user management
+const DevNotes = () => {
+  const [notesOpen, setNotesOpen] = useState(false);
+  return (
+    <Fragment>
+      <hr css={{ height: '1px', width: '100%' }} />
+      <button onClick={() => setNotesOpen(!notesOpen)}>
+        {notesOpen ? 'v' : '^'}
+      </button>
+      <Collapse isOpened={notesOpen}>
+        <h2>todo</h2>
         <ul>
-          <li>ownership + visibility</li>
+          <li>style combatant</li>
+          <li>evaluate viability of controlling via scrolling</li>
+          <li>move more updators into useCombatantAction</li>
+          <li>loading control + Suspense</li>
+          <li>react-flip-toolkit</li>
+          <li>
+            user management
+            <ul>
+              <li>ownership + visibility</li>
+            </ul>
+          </li>
+          <li>events + event log</li>
+          <li>rolling system</li>
+          <li>effects + expenditures</li>
+          <li>continue adding usePermissions for combatant sections</li>
+          <li>combatantitem refactor into molecules</li>
+          <li>configure combatant</li>
+          <li>convert to dotenv setup (firebase)</li>
         </ul>
-      </li>
-      <li>events + event log</li>
-      <li>rolling system</li>
-      <li>effects + expenditures</li>
-    </ul>
-    <h2>bugs</h2>
-    <ul>
-      <li>
-        input caret position bounces around (see{' '}
-        <a href="https://github.com/facebook/react/issues/955#issuecomment-447440342">
-          issue
-        </a>
-        )
-      </li>
-    </ul>
-  </Fragment>
-);
+        <h2>bugs</h2>
+        <ul>
+          <li>
+            input caret position bounces around (see{' '}
+            <a href="https://github.com/facebook/react/issues/955#issuecomment-447440342">
+              issue
+            </a>
+            )
+          </li>
+        </ul>
+      </Collapse>
+    </Fragment>
+  );
+};
 
 export default DevNotes;
